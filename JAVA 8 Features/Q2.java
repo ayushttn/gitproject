8868a0a656c2d5051e52df9b76450e90af4809d3
ayushttn <ayush.tyagi2@tothenew.com>
2022-02-22 00:58:15 +0530

    Java_8_Features ; , -

--- JAVA 8 Features/Q2.java
+++ JAVA 8 Features/Q2.java
@@ -7,6 +7,10 @@ public class Q2{
         System.out.println("Addition of the given numbers:");
         return a+b;
     }
+    public int sub(int a, int b){
+        System.out.println("Subtraction of a from b:");
+        return b-a;
+    }
     public static int multiply(int a, int b){
         System.out.println("Product of the given numbers:");
         return a*b;
@@ -15,7 +19,9 @@ public class Q2{
     public static void main(String[] args) {
         myInterface m1 = new Q2()::add;
         System.out.println(m1.Ope(4,5));
-        myInterface m2 = Q2::multiply;
-        System.out.println(m2.Ope(3,2));
+        myInterface m2 = new Q2()::sub;
+        System.out.println(m2.Ope(3,5));
+        myInterface m3 = Q2::multiply;
+        System.out.println(m3.Ope(3,2));
     }
 }
